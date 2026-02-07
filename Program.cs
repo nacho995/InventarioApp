bf0c2e68130b4780d1d89caa625ea98ef84c8d3c
nacho <ignaciodalesio1995@gmail.com>
2026-02-07 13:38:06 +0100

    Production redirection https

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using InventarioApp.Data;
 using MudBlazor.Services;
 using InventarioApp.Services;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<StockService>();
 
@@ -32,11 +30,9 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
